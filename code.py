80a66f0de8ffce150ba7a05912cb64f1e4bdef37
Andrey Semakin <and-semakin@ya.ru>
2017-05-08 18:20:50 +0500

    Addedd settings.py file to split code and settings

--- code.py
+++ code.py
@@ -5,31 +5,7 @@ import win32api, win32con
 import win32com.client
 from PIL import ImageOps
 from numpy import *
-
-# Debug mode; set to True to get tons of info
-debug = False
-
-# Offset for screen_grab
-x_pad = 622
-y_pad = 99
-width = 127
-height = 248
-
-# Starting points count
-points = 0
-
-# Name of dir store screenshots
-cur_time = time.strftime('%Y-%m-%d %H-%M-%S', time.gmtime())
-
-# Colors
-tree_color = (161, 116, 56)
-you_scored_label_color = (77, 77, 77)
-
-# Speed settings
-limit = 650
-sleep_before_limit = .035
-sleep_after_limit = .040
-sleep_error = .001
+import settings
 
 # VBScript objects to push keys
 shell = win32com.client.Dispatch("WScript.Shell")
---
+++ settings.py
@@ -0,0 +1,24 @@
+# Debug mode; set to True to get tons of info
+debug = False
+
+# Offset for screen_grab
+x_pad = 622
+y_pad = 99
+width = 127
+height = 248
+
+# Starting points count
+points = 0
+
+# Name of dir store screenshots
+cur_time = time.strftime('%Y-%m-%d %H-%M-%S', time.gmtime())
+
+# Colors
+tree_color = (161, 116, 56)
+you_scored_label_color = (77, 77, 77)
+
+# Speed settings
+limit = 650
+sleep_before_limit = .035
+sleep_after_limit = .040
+sleep_error = .001
