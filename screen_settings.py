8fcc4795dbf9022b7f0d32f5d9f0d771d727d0a7
Andrey Semakin <and-semakin@ya.ru>
2017-05-08 20:01:15 +0500

    Move screen dependent settings and constants to screen_settings.py

--- screen_settings.py
+++ screen_settings.py
@@ -3,3 +3,11 @@ x_pad = 622
 y_pad = 99
 width = 127
 height = 248
+
+# Columns to check
+left_column = 8
+right_column = 113
+
+# Rows to check game over
+row_gameover_from = 140
+row_gameover_to = 140
